20db3c58de4c5a4d829e0e0e5f5f9381241f1b85
BuildTools <unconfigured@null.spigotmc.org>
2017-08-15 15:06:20 +0200

    JSON settings v1.0 commit 2

--- constants.py
+++ constants.py
@@ -1,10 +1,13 @@
 import pygame
+import settings_loader
+
+settings_loader.load() # naloži settings.json
 
 if not pygame.font.get_init():
     pygame.font.init()
 
-window_width = 1000  # širina okna
-window_height = 1000  # višina okna
+window_width = settings_loader.settings["window_width"]  # širina okna
+window_height = settings_loader.settings["window_height"]  # višina okna
 #spreminjanje teh števil bo vplivalo na izgled grafa in okna
 
 # graf je le del okna, zato potrebuje svojo širino in višino
@@ -13,13 +16,15 @@ graph_width = window_width*0.95
 # spreminjanje teh števil bo vplivalo na grafu dostopno polje
 
 
-font = pygame.font.SysFont("monospace", 25, True)  # font s katerim bo izpisano besedilo in števila
+font = pygame.font.SysFont(settings_loader.settings["font_name"], settings_loader.settings["font_size"],
+                           settings_loader.settings["font_bold"], settings_loader.settings["font_italic"])
+# font s katerim bo izpisano besedilo in števila
 
 # števila, ki vplivajo na izačune točk
-start_graph_number = 0  # število v sredini grafa
-max_graph_number_x = 5  # največje prikazano število v osi x
-max_graph_number_y = 5  # največje prikazano število v osi y
-graph_calculation_interval = 0.0001  # interval med točkami. Računalnik bo točko izračunal vsakih graph_calculation_interval števil
+start_graph_number = settings_loader.settings["start_graph_number"]  # število v sredini grafa
+max_graph_number_x = settings_loader.settings["max_graph_number_x"]  # največje prikazano število v osi x
+max_graph_number_y = settings_loader.settings["max_graph_number_y"]  # največje prikazano število v osi y
+graph_calculation_interval = settings_loader.settings["interval"]  # interval med točkami. Računalnik bo točko izračunal vsakih graph_calculation_interval števil
                                    # manjše število pomeni lepši graf
 
 
@@ -31,12 +36,12 @@ graph_unit_size_x = graph_width / ((max_graph_number_x - start_graph_number)*2+1
 
 line_y_start = (window_width*0.5, window_height*0.02)  # začetna pozicija osi y
 line_x_start = (window_width*0.02, line_y_start[1]+graph_height/2)  # začetna pozicija osi x
-line_width = 3 # debelina osi v pikslih
+line_width = settings_loader.settings["line_width"] # debelina osi v pikslih
 # števila bodo spremenila pozicije osi x in y
 
 
-show_numbers = True  # ali naj računalnik pokaže pomožna števila
-show_multples_of = 1  # računalnik bo pomožno število prikazal le, če je deljivo z show_multiples_of
+show_numbers = settings_loader.settings["show_numbers"]  # ali naj računalnik pokaže pomožna števila
+show_multples_of = settings_loader.settings["show_multiples_of"]  # računalnik bo pomožno število prikazal le, če je deljivo z show_multiples_of
 
-show_help_lines = True  # ali naj računalnik pokaže pomožne črte
-help_line_width = 1  # debelina pomožnih črt v pikslih
+show_help_lines = settings_loader.settings["show_help_lines"]  # ali naj računalnik pokaže pomožne črte
+help_line_width = settings_loader.settings["help_line_width"]  # debelina pomožnih črt v pikslih
